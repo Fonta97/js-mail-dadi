1a0320098563858602048e7a3d7fc2a52503bd20
Fonta97 <fontana.alessandro97@gmail.com>
2023-11-22 16:34:40 +0100

    config: declaration variables

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -1,11 +1,19 @@
 // generare un numero random tra 1 e 6 sia per il giocatore che per il computer e dire chi vince
-    // creo array utente e computer
-    // creo variabile numero random
-    // genero numero random con Math.random()*6 +1
-    // associo ad utente e poi a computer
-    
-    // IF in cui controllo se numero utente > computer
-        // stampo messaggio hai vinto
-    // ALTRIMENTI SE utente === computer controllo
-    // se è vero stampo PAREGGIO
-    // altrimenti HAI PERSO
+// creo array utente e computer
+// creo variabile numero random
+// genero numero random con Math.random()*6 +1
+// associo ad utente e poi a computer
+
+// IF in cui controllo se numero utente > computer
+// stampo messaggio hai vinto
+// ALTRIMENTI SE utente === computer controllo
+// se è vero stampo PAREGGIO
+// altrimenti HAI PERSO
+
+let user;
+let computer;
+const authorized = [computer, user];
+let dadoN = 0;
+
+dadoN = Math.floor(Math.random() * 6 + 1);
+console.log
