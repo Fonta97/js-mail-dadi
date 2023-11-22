067b4df982e63dfa24bbc4b080165bd608a218f9
Fonta97 <fontana.alessandro97@gmail.com>
2023-11-22 14:46:55 +0100

    email inizio

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// chiedo mail all'utente
+// creo prompt richiesta mail 
+// assegno alla variabile email il risultato del prompt
+// controllo che sia nella lista di chi può accedere
+// creo array utenti con permesso di accesso
+// controllo che possa accedere
+//  se può accedere stampo messagio conferma accesso
+// ALTRIMENTI stampo messaggio negazione accesso
+
+
+const mail = prompt("Inserisci e-mail:")
+console.log("utente prompt email", mail);
+
+if
+
+
+document.getElementById("esito").innerHTML = "Puoi accedere";
